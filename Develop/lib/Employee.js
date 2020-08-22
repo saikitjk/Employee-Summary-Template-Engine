25e80ed73501de43622dd9691c28e7f3bdccb663
JK <saikitjk@gmail.com>
2020-08-21 21:36:31 -0700

    created employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,28 @@
 // TODO: Write code to define and export the Employee class
+class Employee{
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getRole(){
+        return "Employee";
+    }
+
+    getId(){
+        return this.name;
+    }
+
+    getEmail(){
+        return this.name;
+    }
+
+}
+
+
+module.exports = Employee;
